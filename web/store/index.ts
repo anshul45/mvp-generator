e073b4c30a04f859bc5cbbb0a4b5d4ebb339239d
Abhilasha <singhabhilasha4042@gmail.com>
2025-02-20 09:38:08 +0530

    removes web

--- web/store/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as useCounterStore } from './counterStore';
-export { default as useUserStore } from './userStore';
--- web/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './files';
-export * from './functions';
-export * from './string';
-export * from './datetime';
-export * from './safety';
